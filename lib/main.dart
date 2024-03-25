3ed45ff1a631f0e3528ae3235029c60d7b08fe59
심상익 <tkddlrtla@naver.com>
2024-03-25 15:20:00 +0900

    Feat: push 알림 앱 데이터에 저장 및 알림 탭에 목록 표시

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
+import 'dart:convert';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print('Handling a background message: ${message.messageId}');
+  print('백그라운드 메세지 제목: ${message.notification?.title}');
+  print('백그라운드 메세지 내용: ${message.notification?.body}');
+
+  if (message.notification != null) {
+    await saveNotification(
+        message.notification!.title!, message.notification!.body!);
+  }
+}
+
+Future<void> saveNotification(String title, String body) async {
+  final prefs = await SharedPreferences.getInstance();
+
+  // 알림 리스트를 가져옴. 없으면 빈 리스트를 사용
+  final String storedNotifications = prefs.getString('notifications') ?? '[]';
+  final List<dynamic> notifications = json.decode(storedNotifications);
+
+  // 새 알림을 리스트에 추가
+  notifications.add({"title": title, "body": body});
+
+  // 업데이트된 알림 리스트를 다시 저장
+  await prefs.setString('notifications', json.encode(notifications));
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   const AndroidNotificationChannel channel = AndroidNotificationChannel(
     'high_importance_channel', // id
@@ -148,28 +178,97 @@ class LoginPage extends StatelessWidget {
   }
 }
 
-class MainScreen extends StatelessWidget {
+class NotificationsPage extends StatefulWidget {
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  List<Map<String, dynamic>> notifications = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadNotifications();
+  }
+
+  Future<void> _loadNotifications() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String storedNotifications = prefs.getString('notifications') ?? '[]';
+    final List<dynamic> notificationsList = json.decode(storedNotifications);
+
+    setState(() {
+      notifications = notificationsList.cast<Map<String, dynamic>>();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notifications'),
+      ),
+      body: ListView.builder(
+        itemCount: notifications.length,
+        itemBuilder: (context, index) {
+          final notification = notifications[index];
+          return ListTile(
+            title: Text(notification['title']),
+            subtitle: Text(notification['body']),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _pages = [HomeWidget(), NotificationsPage()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Main Screen'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () {
-              // Simulate logout
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => LoginPage()),
-              );
-            },
-          ),
-        ],
       ),
       body: Center(
-        child: Text('메인화면입니다'),
+        child: _pages.elementAt(_selectedIndex),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('메인화면입니다'),
     );
   }
 }
